5e576673928a56fba11eb7dd22c4ed2d92e71443
Zundel <Andrew.Zundel@us.crowncastle.com>
2017-08-07 12:21:59 -0400

    initial quickstart angular set-up

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+  providers: [HeroService]
+})
+
+export class HeroesComponent implements OnInit  {
+  title = 'Tour of Heroes';
+  hero: Hero = {id: 1, name: 'Windstorm'};
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  addHero(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.createHero(name).then(hero => {
+      this.heroes.push(hero);
+      this.selectedHero = null;
+    });
+  }
+
+  goToDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+}
+
+
